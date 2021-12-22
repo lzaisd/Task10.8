4949c9a77d07e9f526f5f9efcbd31d5a563358de
Elizaveta Sidorova <elizavetka.sidorova@bk.ru>
2021-12-23 00:09:56 +0300

    Adding tests

--- src/ru/vsu/cs/sidorovaei/mains/testsMain.java
+++ src/ru/vsu/cs/sidorovaei/mains/testsMain.java
@@ -10,6 +10,15 @@ public class testsMain {
         ArrayList<Line> listOfLines1 = Logic.readListFromFile("input1.txt");
         ArrayList<Line> answer1 = Logic.readListFromFile("answer1.txt");
         test(Logic.getListOfMergedLines(listOfLines1), answer1);
+        ArrayList<Line> listOfLines2 = Logic.readListFromFile("input2.txt");
+        ArrayList<Line> answer2 = Logic.readListFromFile("answer2.txt");
+        test(Logic.getListOfMergedLines(listOfLines2), answer2);
+        ArrayList<Line> listOfLines3 = Logic.readListFromFile("input3.txt");
+        ArrayList<Line> answer3 = Logic.readListFromFile("answer3.txt");
+        test(Logic.getListOfMergedLines(listOfLines3), answer3);
+        ArrayList<Line> listOfLines4 = Logic.readListFromFile("input4.txt");
+        ArrayList<Line> answer4 = Logic.readListFromFile("answer4.txt");
+        test(Logic.getListOfMergedLines(listOfLines4), answer4);
     }
     public static void test(ArrayList<Line> listOfLines, ArrayList<Line> answer) {
         System.out.print("Полученный ответ: ");
